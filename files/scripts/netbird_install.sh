afd4fcb7cfbb9e5f79685698d188d15d04efa931
Peter Liveyns <peter.liveyns@gmail.com>
2026-01-01 11:11:30 +0100

    Debug netbird install

--- files/scripts/netbird_install.sh
+++ files/scripts/netbird_install.sh
@@ -9,4 +9,4 @@ tar xzf ./netbird_${VERSION}.tar.gz
 mv netbird /usr/bin/netbird
 chown root:root /usr/bin/netbird
 chmod +x /usr/bin/netbird
-dnf install -y https://github.com/netbirdio/netbird/releases/download/v${VERSION}/netbird-ui_${VERSION}_linux_amd64.rpm
+dnf install -y --skip-broken https://github.com/netbirdio/netbird/releases/download/v${VERSION}/netbird-ui_${VERSION}_linux_amd64.rpm
