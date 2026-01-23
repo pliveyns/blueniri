4d623ad280ef9d86c7aee9387e4ea50787cd7846
Peter Liveyns <peter.liveyns@gmail.com>
2026-01-23 21:22:27 +0100

    Debug MegaSync module

--- files/scripts/megasync_install.sh
+++ files/scripts/megasync_install.sh
@@ -3,8 +3,10 @@
 set -oue pipefail
 
 # MegaSync binary install from mega.nz website
-FEDORAVERSION=Fedora_43
-MEGASYNCVERSION=megasync-6.1.1-1.1.x86_64
-NAUTILUSMEGASYNCVERSION=nautilus-megasync-5.4.0-1.1.x86_64
-rpm --install https://mega.nz/linux/repo/${FEDORAVERSION}/x86_64/${MEGASYNCVERSION}.rpm
-rpm --install https://mega.nz/linux/repo/${FEDORAVERSION}/x86_64/${NAUTILUSMEGASYNCVERSION}.rpm
+#FEDORAVERSION=Fedora_43
+#MEGASYNCVERSION=megasync-6.1.1-1.1.x86_64
+#NAUTILUSMEGASYNCVERSION=nautilus-megasync-5.4.0-1.1.x86_64
+#rpm --install https://mega.nz/linux/repo/${FEDORAVERSION}/x86_64/${MEGASYNCVERSION}.rpm
+#rpm --install https://mega.nz/linux/repo/${FEDORAVERSION}/x86_64/${NAUTILUSMEGASYNCVERSION}.rpm
+
+wget https://mega.nz/linux/repo/Fedora_43/x86_64/megasync-Fedora_43.x86_64.rpm && sudo dnf install "$PWD/megasync-Fedora_43.x86_64.rpm"
