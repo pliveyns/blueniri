95705303e9ed64d73c75bd35cd7397204de39b9b
Peter Liveyns <peter.liveyns@gmail.com>
2026-04-21 19:25:32 +0200

    Update netbird version

--- files/scripts/netbird_install.sh
+++ files/scripts/netbird_install.sh
@@ -3,7 +3,7 @@
 set -oue pipefail
 
 # Netbird binary install from github
-VERSION=0.65.3
+VERSION=0.69.0
 rpmkeys --import https://pkgs.netbird.io/yum/repodata/repomd.xml.key
 curl -L -o ./netbird_${VERSION}.tar.gz https://github.com/netbirdio/netbird/releases/download/v${VERSION}/netbird_${VERSION}_linux_amd64.tar.gz
 tar xzf ./netbird_${VERSION}.tar.gz
