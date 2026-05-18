d093cde9b6f3f17cb2a1b525aa4a94419122c2ac
Peter Liveyns <peter.liveyns@gmail.com>
2026-05-18 21:09:07 +0200

    Correction to script bootc-sudo-acces.sh

--- files/scripts/bootc-sudo-access.sh
+++ files/scripts/bootc-sudo-access.sh
@@ -5,7 +5,7 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
-echo 'Set sudo rights for bootc command without password"
-cat << 'EOF' > /etc/sudoers.d/001-bootc
+echo 'Set sudo rights for bootc command without password'
+cat <<'EOF' >/etc/sudoers.d/001-bootc
 %wheel ALL=(ALL) NOPASSWD: /usr/bin/bootc update, /usr/bin/bootc upgrade, /usr/bin/bootc status, /usr/bin/bootc status --booted
 EOF
